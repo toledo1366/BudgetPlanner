c2f8e2de98d76635dfac1f00242259375ae0cbd0
toledo1366 <46579344+toledo1366@users.noreply.github.com>
2023-12-14 19:42:16 +0100

    Setting up mvvmcross framework

---
+++ BudgetPlanner.Core/App.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using MvvmCross;
+using MvvmCross.ViewModels;
+
+namespace BudgetPlanner.Core
+{
+	//Tu zaczyna się nasza apka, TODO trzeba skonfigurować start apki oraz w tym miejscu ustawić kontener DI.
+	public class App : MvxApplication
+	{
+
+    }
+}
+
