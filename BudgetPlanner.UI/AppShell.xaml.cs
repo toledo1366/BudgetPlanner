84ff255aec10d5d57640e1c846cd7789622a0d97
toledo1366 <46579344+toledo1366@users.noreply.github.com>
2024-01-10 20:38:55 +0100

    migrating project to maui

---
+++ BudgetPlanner.UI/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace BudgetPlanner.UI;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
---
+++ BudgetPlannet.Core/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace BudgetPlannet.Core;
+
+public class Class1
+{
+
+}
+
